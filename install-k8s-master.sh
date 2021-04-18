2e78b26d0306f2ad3bbe779915e2cffb190203fb
Nila Prasad <Nilaprasad1994@gmail.com>
2021-04-18 18:27:12 -0400

    Modified service files for loadbalancing.

--- install-k8s-master.sh
+++ install-k8s-master.sh
@@ -23,3 +23,5 @@ sudo kubeadm init
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
