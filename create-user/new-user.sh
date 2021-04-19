2b6797c2fa008e9bb547590865b653b82e7f4da1
Nila Prasad <Nilaprasad1994@gmail.com>
2021-04-18 20:05:08 -0400

    HPA files added.

--- create-user/new-user.sh
+++ create-user/new-user.sh
@@ -8,6 +8,8 @@ sudo openssl req -new -key employee.key -out employee.csr -subj "/CN=employee"
 sudo export CA_LOCATION=/etc/kubernetes/pki/
 sudo openssl x509 -req -in employee.csr -CA CA_LOCATION/ca.crt -CAkey CA_LOCATION/ca.key -CAcreateserial -out employee.crt -days 500
 sudo mkdir .certs && sudo mv employee.crt employee.key .certs
+sudo chmod 777 /home/employee/.certs/employee.crt
+sudo chmod 777 /home/employee/.certs/employee.key
 
 kubectl config set-credentials employee --client-certificate=/home/employee/.certs/employee.crt  --client-key=/home/employee/.certs/employee.key
 kubectl config set-context employee-context --cluster=kubernetes --user=employee
